0b5763cb9d164430bd7276b9c62f431bc2d86ec3
szabolaszlo <laszlo.szabo@shoprenter.hu>
2025-01-17 16:03:00 +0100

    Add VueJS2 MS

---
+++ vuejs2_ms_1/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {VueLoaderPlugin} = require('vue-loader');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            vue$: 'vue/dist/vue.esm.js',
+        },
+        extensions: ['*', '.js', '.vue', '.json'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new VueLoaderPlugin(),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        compress: true,
+        port: 8081,
+    },
+};

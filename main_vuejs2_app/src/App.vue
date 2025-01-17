095553b01c98a8a5e79d848695903d9d31f7e1c8
szabolaszlo <laszlo.szabo@shoprenter.hu>
2025-01-17 15:56:52 +0100

    Main VueJS2 app

---
+++ main_vuejs2_app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue 2!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+}
+</style>

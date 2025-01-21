8c92e638bb6b80e92689281928fa71a2bd348c0e
szabolaszlo <laszlo.szabo@shoprenter.hu>
2025-01-21 14:42:24 +0100

    collect remote run time

--- dynamic_main_vuejs2_app/src/App.vue
+++ dynamic_main_vuejs2_app/src/App.vue
@@ -74,7 +74,7 @@ export default {
   async mounted() {
     try {
       // 1) Lekérjük a /remotes.json tartalmát
-      const response = await fetch('https://run.mocky.io/v3/ef0959fb-6c06-4aa6-8c97-084e930ce8db');
+      const response = await fetch('https://run.mocky.io/v3/bfdbd135-4185-4545-afe9-0856215629bc');
       if (!response.ok) {
         throw new Error(`Nem sikerült lekérni a /remotes.json-t: ${response.status}`);
       }
